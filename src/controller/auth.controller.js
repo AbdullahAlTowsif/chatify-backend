008f58d0de18264cc12d17a7718824d31beb94f6
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-03-27 23:35:42 +0600

    login & logout added

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -59,3 +59,42 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatch) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
+        generateToken(user._id, res);
+        res.status(200).json({
+            message: "Login successful",
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.error("Error in login:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const logout = (_, res) => {
+    try {
+        res.clearCookie("token");
+        res.status(200).json({ message: "Logout successful" });
+    } catch (error) {
+        console.error("Error in logout:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
