e75d33bce01759a3f1a9bfe2d0786efdf86b1036
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-03-31 01:41:52 +0600

    cors added

--- src/server.js
+++ src/server.js
@@ -4,11 +4,17 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from './lib/db.js';
 import { ENV } from './lib/env.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({
+  // origin: 'http://localhost:5173',
+  origin: ENV.CLIENT_URL,
+  credentials: true,
+}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
