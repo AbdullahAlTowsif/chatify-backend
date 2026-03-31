865e4ce3a7e02dc6a11efbe2ae8824de2c1a6567
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2026-04-01 02:37:50 +0600

    express limit added

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 const app = express();
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit: '5mb'}));
 app.use(cors({
   // origin: 'http://localhost:5173',
   origin: ENV.CLIENT_URL,
